eab99affa66238b89adf345115ffcc635fa543e2
Farhan Sheikh <ipadnoodle@gmail.com>
2020-09-15 13:46:09 -0400

    Fixed pausing

--- Pause.gd
+++ Pause.gd
@@ -1,7 +1,14 @@
-extends Control
+extends ColorRect
+
+var pause_state;
 
 func _input(event):
 	if (event.is_action_pressed("ui_cancel")):
-		var pause_state = not get_tree().paused;
-		get_tree().paused = pause_state;
-		self.visible = pause_state; 
+		invert_pause();
+		
+
+func invert_pause():
+	pause_state = not get_tree().paused;
+	get_tree().paused = pause_state;
+	print(pause_state)
+	self.visible = pause_state;
--- Player.gd
+++ Player.gd
@@ -3,11 +3,11 @@ extends KinematicBody2D
 var velocity = Vector2();
 var axis = Vector2();
 var speed = 165;
-var jump_height = -300;
+var jump_height = -400;
 var death_count = 0;
 var last_movement = 0;
 
-var dash_speed = 150;
+var dash_speed = 250;
 var dash_charged = true;
 var is_dashing = false;
 
